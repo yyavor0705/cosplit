8e80de86797a3005e9744c8c5ef52735f3df53f6
Yura <y0507yavor@gmail.com>
2021-08-04 14:36:14 +0300

    Implementation of https://github.com/yyavor0705/cosplit/issues/9 Add group event delete functionality

--- src/components/events/EventsList.tsx
+++ src/components/events/EventsList.tsx
@@ -7,17 +7,17 @@ const DATA = [
     title: "This is event 1",
     costs_data: [
       {
-        id: 1,
+        id: 11,
         participantName: "user1",
         costs: -10.05
       },
       {
-        id: 1,
+        id: 12,
         participantName: "user2",
         costs: 10.05
       },
       {
-        id: 1,
+        id: 13,
         participantName: "user3",
         costs: 1218.05
       },
@@ -28,17 +28,17 @@ const DATA = [
     title: "This is event 2",
     costs_data: [
       {
-        id: 1,
+        id: 21,
         participantName: "user12",
         costs: -104.05
       },
       {
-        id: 1,
+        id: 22,
         participantName: "user22",
         costs: 140.05
       },
       {
-        id: 1,
+        id: 23,
         participantName: "user32",
         costs: -4118.05
       },
@@ -49,17 +49,17 @@ const DATA = [
     title: "This is event 3",
     costs_data: [
       {
-        id: 1,
+        id: 31,
         participantName: "user13",
         costs: -10.05
       },
       {
-        id: 1,
+        id: 32,
         participantName: "user23",
         costs: -102.05
       },
       {
-        id: 1,
+        id: 33,
         participantName: "user33",
         costs: 1844.05
       },
@@ -69,15 +69,23 @@ const DATA = [
 
 const EventList: React.FC = () => {
   const [expandedPanel, setExpandedPanel] = useState("");
+  const [eventsList, setEventsList] = useState(DATA)
 
   const handleChange = (panelKey: string, isExpanded: boolean) => {
     setExpandedPanel(isExpanded ? panelKey : "");
   };
+  
+  const handleDeleteEvent = (eventKey: string) => {
+    const newList = eventsList.filter((element) => {
+      return element.id !== eventKey;
+    })
+    setEventsList(newList);
+  }
 
   return (
     <Fragment>
       {
-        DATA.map(ev => {
+        eventsList.map(ev => {
             console.log("Expanded panel is " + expandedPanel)
             return (
               <EventListItem key={ev.id}
@@ -87,6 +95,7 @@ const EventList: React.FC = () => {
                              details={"Details"}
                              onChangeHandler={handleChange}
                              shortDetailsData={ev.costs_data}
+                             onDelete={handleDeleteEvent}
               />
             );
           }
